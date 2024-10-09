43c5ffde5a0f5a6b7cdae4332a713f794e2e0a79
Wojtek9388 <148169378+Wojtek9388@users.noreply.github.com>
2024-10-09 12:19:02 -0700

    Create payload.py

---
+++ payload.py
@@ -0,0 +1,11 @@
+import random
+def A():
+    this = "balls"
+    var = str(random.randint(1, 99999999))
+    repeatammount = var + this
+    count = 1
+    for i in repeatammount:
+        print(count)
+        count += 1
+
+A()
